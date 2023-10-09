533bf2e3d6d6eacc15e4f4ca4bd3f3bfead070d0
Lukas Schmiedgen <lukas.schmiedgen@nm-tec.de>
2023-10-09 13:21:57 +0200

    implemented for version of OrderApplicationService.createOrder

---
+++ edu-ms-fos/order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderTrackCommandHandler.java
@@ -0,0 +1,17 @@
+package com.food.ordering.system.order.service.domain;
+
+import com.food.ordering.system.order.service.domain.dto.track.TrackOrderQuery;
+import com.food.ordering.system.order.service.domain.dto.track.TrackOrderResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Component
+public class OrderTrackCommandHandler {
+
+    @Transactional
+    public TrackOrderResponse trackOrder(TrackOrderQuery trackOrderQuery) {
+
+    }
+}
--- edu-ms-fos/order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Product.java
+++ edu-ms-fos/order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Product.java
@@ -8,6 +8,10 @@ public class Product extends BaseEntity<ProductId> {
     private String name;
     private Money price;
 
+    public Product(ProductId productId) {
+        super.setId(productId);
+    }
+
     public Product(ProductId productId, String name, Money price) {
         super.setId(productId);
         this.name = name;
