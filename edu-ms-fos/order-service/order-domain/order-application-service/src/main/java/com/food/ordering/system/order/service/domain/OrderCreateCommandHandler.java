0cf66b438a82f475fb99cd184025076ea2fd1e55
Lukas Schmiedgen <lukas.schmiedgen@nm-tec.de>
2023-10-09 20:43:02 +0200

    added base structure and mock data for OrderApplicationServiceTest

--- edu-ms-fos/order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderCreateCommandHandler.java
+++ edu-ms-fos/order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderCreateCommandHandler.java
@@ -42,6 +42,6 @@ public class OrderCreateCommandHandler {
 
         orderCreatedPaymentRequestMessagePublisher.publish(orderCreatedEvent);
 
-        return orderDataMapper.orderToCreateOrderResponse(orderCreatedEvent.getOrder());
+        return orderDataMapper.orderToCreateOrderResponse(orderCreatedEvent.getOrder(), "Order Created Successfully");
     }
 }
